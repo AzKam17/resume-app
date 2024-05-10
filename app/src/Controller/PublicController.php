b050e7378f7167a5333e2e9e3af64d672ec40ccb
azk <aziz.kamagate@local.lan>
2024-05-10 08:09:02 +0000

    Separation des routes

--- app/src/Controller/PublicController.php
+++ app/src/Controller/PublicController.php
@@ -3,15 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class PublicController extends AbstractController
 {
-    #[Route('/app', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_public')]
+    public function public(): Response
     {
-        return $this->render('app/index.html.twig', [
+        return $this->render('public/index.html.twig', [
             'title' => 'HomeController',
         ]);
     }
